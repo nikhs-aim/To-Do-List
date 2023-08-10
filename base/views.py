accb0857429da7072376a78c861b0fe3a15a1faa
Nikhitha R karanth <nikhitharkaranth@gmail.com>
2023-08-10 22:36:33 +0530

    adding search

--- base/views.py
+++ base/views.py
@@ -52,6 +52,13 @@ class tasklist(LoginRequiredMixin, ListView):
             user=self.request.user)       #
         context['count'] = context['tasks'].filter(
             complete=False).count()       # to know the incomplete items
+
+        search_input = self.request.GET.get('search-area') or ''
+        if search_input:
+            context['tasks'] = context['tasks'].filter(
+                title__startswith=search_input)
+
+        context['search_input'] = search_input
         return context
 
 
